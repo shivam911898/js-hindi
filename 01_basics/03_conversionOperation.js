ba4ffcd391ad6e6cd552e61d7fc80819e93a6074
shivam kumar yadav <b230027@nitsikkim.ac.in>
2025-03-21 07:54:48 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,28 @@
+let score = "33"
+
+//console.log(typeof score);
+//console.log(typeof(score));
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+
+// when we convert in a dataype in number 
+// "33"=> 33
+// "33abc"=> NaN
+// true=> 1; false=> 0
+
+/*
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+*/
+
+// 1 => true; 0=> false
+// "" => false
+// "hitesh" => true
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(typeof stringNumber);
+
