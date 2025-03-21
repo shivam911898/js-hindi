82cb625af682cece673f08232c687968d4f5d229
shivam kumar yadav <b230027@nitsikkim.ac.in>
2025-03-21 09:09:58 +0000

    conversionOperation2

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -22,7 +22,53 @@ console.log(booleanIsLoggedIn);
 // "" => false
 // "hitesh" => true
 
+
+/*
 let someNumber = 33
 let stringNumber = String(someNumber)
 console.log(typeof stringNumber);
+*/
 
+// ************************** Operations *****************************
+
+let value = 3
+let negValue = -value
+//console.log(negValue);
+
+/*
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**4);
+console.log(2/2);
+console.log(2%5);
+*/
+
+let str1= "hello"
+let str2= "shivam"
+let str3= str1 + str2
+//console.log(str3);
+
+/*
+console.log("1"+2);
+console.log(1+"2");
+console.log(1 + 2+ "2");
+console.log("1" + 2+ 2);
+*/
+
+
+
+// console.log((3+4)*5%3)
+
+//console.log(true);
+// console.log(+"")
+
+let num1, num2, num3
+
+num1=num2=num3=4
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
+
+// link to study https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Increment
