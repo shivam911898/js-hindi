49ddffa966008bee40709d0e5cfa33c55751d1c8
shivam kumar yadav <b230027@nitsikkim.ac.in>
2025-03-21 07:28:33 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,19 @@
+const accountId=144553
+let accountEmail= "shivam@google.com"
+var accountPassword = "12345"
+accountCity= "Jaipur"
+
+//accountId = 2
+accountEmail= "abc.com"
+accountPassword= "abcd"
+accountCity= "Ballia"
+let accountState;
+
+console.log(accountId)
+
+console.table([accountId, accountEmail,accountPassword,accountCity,accountState]);
+
+/*
+Prefer not to use var
+because of in block scope and functional scope
+*/
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,21 @@
+"use strict"; // treat all JS code as newer version
+
+// alert( 3+3 ) // we are using nodejs, not browser
+
+let name= "shivam"
+let age = 17
+let isLoggedIn = false
+
+// number => 2 to the power of 53
+// bigint
+// string=> ""
+// boolean=> true/false
+// null=> standalone value
+// undefined=> 
+// symbol=> unique
+
+
+// object
+
+console.log(typeof null); // undefined 
+console.log(typeof undefined); // object
